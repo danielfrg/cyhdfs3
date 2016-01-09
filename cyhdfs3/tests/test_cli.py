82bc2b2cf63e61bc7282bb61e108a6815e8acd78
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-01-09 00:51:38 -0600

    Add template for cli test

---
+++ cyhdfs3/tests/test_cli.py
@@ -0,0 +1,12 @@
+import click
+from click.testing import CliRunner
+
+from cyhdfs3 import cli
+from utils import *
+
+
+def test_create_file_list(hdfs):
+    runner = CliRunner()
+    result = runner.invoke(cli.ls)
+    # assert result.exit_code == 0
+    # assert result.output == 'Hello Peter!\n'

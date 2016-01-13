582c02c915579c9eb21a43fdb7c0c498a65dfa69
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-01-12 23:13:08 -0600

    Commented pyximport

--- cyhdfs3/__init__.py
+++ cyhdfs3/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, absolute_import
 
+# import pyximport; pyximport.install()
 from cyhdfs3.cyhdfs3 import HDFSClient, File, FileInfo, BlockLocation
 
 from ._version import get_versions
